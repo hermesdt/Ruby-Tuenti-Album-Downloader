47e4332ee7b4e30884b695eaaa7b16a281fb54d9
Alvaro Duran <hermesdt@gmail.com>
2011-05-01 23:42:51 +0200

    test

--- test.rb
+++ test.rb
@@ -1,28 +1,21 @@
 require 'rubygems'
 require 'logger'
 require 'mechanize'
+require 'json'
 
 LOGIN_URL = 'https://www.tuenti.com/?m=Login&func=do_login'
 HOME_URL = 'http://www.tuenti.com/?m=home&func=view_home'
 ALBUMS_URL = 'http://www.tuenti.com/?m=Search&func=get_user_custom_albums_for_data_source&ajax=1'
 
-def recursive_download_album(photo_url)
-  photo_page = M.get(photo_url)
-  photo_img = photo_page.images.find{|i| i.node[:alt] =~ /Foto/}
-
-end
-
-
 logger =Logger.new(STDOUT)
 logger.level = Logger::INFO
-M = Mechanize.new{|m| m.log = logger}
+@m = Mechanize.new{|m| m.log = logger}
 
 exit if ARGV.count != 2
 user = ARGV[0]
 pass = ARGV[1]
 
-M.post(LOGIN_URL, {:email => user, :input_password => pass, :timezone => Time.now.utc_offset/3600})
-
-puts M.get(HOME_URL).body
-puts M.get(ALBUMS_URL).body
-
+@m.post(LOGIN_URL, {:email => user, :input_password => pass, :timezone => Time.now.utc_offset/3600})
+#images = @m.get("?m=Albums&func=index&collection_key=3-60526335-5411208&ajax=1").images
+images = @m.get("?m=Photo&func=view_photo&collection_key=3-60526335-5407593-705561026-60526335-1281373232&ajax=1&store=1&ajax_target=canvas").images
+images.each{|i| @m.get(i.src).save}
